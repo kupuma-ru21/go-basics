196a2dcd2560353cff1a0baff8a7551ca40bba30
kupuma-ru21 <kupumaru21@gmail.com>
2023-07-31 17:33:39 +0900

    feat: add calculator package

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"go-basics/calculator"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// static checkをmanualで設定した。
+// https://stackoverflow.com/questions/71101439/how-can-i-configure-the-staticcheck-linter-in-visual-studio-code
+
+// vscode goのextensionが正常に作動しなかった
+// https://formulae.brew.sh/formula/goplsをinstallする必要があった
+
+func main() {
+	godotenv.Load()
+	fmt.Println(os.Getenv("GO_ENV"))
+	fmt.Println(calculator.Offset)
+	fmt.Println(calculator.Sum(1, 2))
+	fmt.Println(calculator.Multiply(1, 2))
+}
