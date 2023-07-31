42d61bf7dbc9be36f6c596fc800d7b179ae91d40
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-01 02:30:24 +0900

    chore: if and for

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
+	"time"
 )
 
 // static checkをmanualで設定した。
@@ -11,80 +11,53 @@ import (
 // vscode goのextensionが正常に作動しなかった
 // https://formulae.brew.sh/formula/goplsをinstallする必要があった
 
-type controller interface {
-	speedUp() int
-	speedDown() int
-}
-
-type vehicle struct {
-	speed       int
-	enginePower int
-}
-
-type bicycle struct {
-	speed      int
-	humanPower int
-}
-
 func main() {
-	v := &vehicle{0, 5}
-	speedUpAndDown(v)
-
-	b := &bicycle{0, 5}
-	speedUpAndDown(b)
-
-	fmt.Println(v)
-
-	var i1 interface{}
-	var i2 any
-	fmt.Printf("%[1]v %[1]T %v\n", i1, unsafe.Sizeof(i1))
-	fmt.Printf("%[1]v %[1]T %v\n", i2, unsafe.Sizeof(i2))
-	checkType(i2)
-	i2 = 1
-	checkType(i2)
-	i2 = "hello"
-	checkType(i2)
-}
-
-func (v *vehicle) speedUp() int {
-	v.speed = v.speed + (10 * v.enginePower)
-	return v.speed
-}
-
-func (v *vehicle) speedDown() int {
-	v.speed = v.speed - (5 * v.enginePower)
-	return v.speed
-}
-
-func (b *bicycle) speedUp() int {
-	b.speed = b.speed + (3 * b.humanPower)
-	return b.speed
-}
-
-func (b *bicycle) speedDown() int {
-	b.speed = b.speed - b.humanPower
-	return b.speed
-}
-
-func speedUpAndDown(c controller) {
-	fmt.Printf("current speed: %v\n", c.speedUp())
-	fmt.Printf("current speed: %v\n", c.speedDown())
-}
-
-func (v *vehicle) String() string {
-	// NOTE: Sprintfは標準出力でなく、単なるstringとして出力される
-	return fmt.Sprintf("Vehicle current speed is %v (enginePower %v)", v.speed, v.enginePower)
-}
-
-func checkType(i any) {
-	switch i.(type) {
-	case nil:
-		fmt.Println("nil")
-	case int:
-		fmt.Println("int")
-	case string:
-		fmt.Println("string")
-	default:
-		fmt.Println("unknown")
+	a := -1
+	if a == 0 {
+		fmt.Println("zero")
+	} else if a > 0 {
+		fmt.Println("positive")
+	} else {
+		fmt.Println("negative")
 	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	var i int
+	for {
+		if i > 3 {
+			break
+		}
+		fmt.Println(i)
+		i += 1
+		time.Sleep(300 * time.Millisecond)
+	}
+
+loop:
+	for i := 0; i < 10; i++ {
+		switch i {
+		case 2:
+			continue
+		case 3:
+			continue
+		case 8:
+			break loop
+		default:
+			fmt.Printf("%v", i)
+		}
+		fmt.Printf("\n")
+	}
+
+	items := []item{{price: 10.}, {price: 20.}, {price: 30.}}
+	for i := range items {
+		items[i].price = items[i].price * 1.1
+	}
+	fmt.Printf("%+v\n", items)
+
+}
+
+type item struct {
+	price float32
 }
