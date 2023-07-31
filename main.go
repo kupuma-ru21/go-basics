d400624d6c2b81d76623f34c120c4b4dee2ad3fd
kupuma-ru21 <kupumaru21@gmail.com>
2023-07-31 20:04:57 +0900

    chore: pointer

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 // static checkをmanualで設定した。
 // https://stackoverflow.com/questions/71101439/how-can-i-configure-the-staticcheck-linter-in-visual-studio-code
@@ -9,11 +12,30 @@ import "fmt"
 // https://formulae.brew.sh/formula/goplsをinstallする必要があった
 
 func main() {
-	// var i int = 2
-	// fmt.Println(i)
-	ui := uint16(2)
-	i := 1
-	fmt.Println(i)
-	fmt.Printf("i: %v %T\n", i, i)
-	fmt.Printf("i: %[1]v %[1]T ui: %[2]v %[2]T\n", i, ui)
+	var ui1 uint16
+	fmt.Printf("memory top address of ui1 is %p\n", &ui1)
+
+	var ui2 uint16
+	fmt.Printf("memory top address of ui2 is %p\n", &ui2)
+
+	var p1 *uint16
+	fmt.Printf("value of p1 is %v\n", p1)
+	p1 = &ui1
+	// value of p1 is equal to memory top address of ui1
+	fmt.Printf("value of p1 is %v\n", p1)
+	fmt.Printf("size of p1 is %d[bytes]\n", unsafe.Sizeof(p1))
+	fmt.Printf("memory top address of p1 is %p\n", &p1)
+	fmt.Printf("value of ui1(dereference) is %v\n", *p1)
+	*p1 = 1
+	fmt.Printf("value of ui1 is %v\n", ui1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1 is %v\n", pp1)
+	fmt.Printf("size of pp1 is %d[bytes]\n", unsafe.Sizeof(pp1))
+	fmt.Printf("memory top address of pp1 is %p\n", &pp1)
+	fmt.Printf("value of p1(dereference) is %v\n", *pp1)
+	fmt.Printf("value of ui1(dereference) is %v\n", **pp1)
+
+	**pp1 = 10
+	fmt.Printf("value of ui1 is %v\n", ui1)
 }
