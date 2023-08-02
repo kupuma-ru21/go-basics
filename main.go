6c63e895bf266b3da4d2583a3ff5c5e4a1d27b26
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-02 22:55:07 +0900

    chore: context

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -14,50 +13,105 @@ import (
 // https://formulae.brew.sh/formula/goplsをinstallする必要があった
 
 func main() {
-	// var rwMu sync.RWMutex
 	// var wg sync.WaitGroup
-	// var c int
-	// wg.Add(4)
-	// go write(&rwMu, &wg, &c)
-	// go read(&rwMu, &wg, &c)
-	// go read(&rwMu, &wg, &c)
-	// go read(&rwMu, &wg, &c)
+	// ctx, cancel := context.WithTimeout(context.Background(), 400*time.Millisecond)
+	// defer cancel()
+	// wg.Add(3)
+	// go subTask(ctx, &wg, "a")
+	// go subTask(ctx, &wg, "b")
+	// go subTask(ctx, &wg, "c")
 	// wg.Wait()
-	// println("finish")
 
-	var wg sync.WaitGroup
-	var c int64
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for j := 0; j < 10; j++ {
-				atomic.AddInt64(&c, 1)
-			}
-		}()
+	// var wg sync.WaitGroup
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
+
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	v, err := criticalTask(ctx)
+	// 	if err != nil {
+	// 		fmt.Printf("critical task cancelled due to: %v\n", err)
+	// 		cancel()
+	// 		return
+	// 	}
+	// 	fmt.Println("success", v)
+	// }()
+
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	v, err := normalTask(ctx)
+	// 	if err != nil {
+	// 		fmt.Printf("normal task cancelled due to: %v\n", err)
+	// 		cancel()
+	// 		return
+	// 	}
+	// 	fmt.Println("success", v)
+	// }()
+
+	// wg.Wait()
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(20*time.Millisecond))
+	defer cancel()
+	ch := deadlineTask(ctx)
+	v, ok := <-ch
+	if ok {
+		fmt.Println(v)
 	}
-	wg.Wait()
-	fmt.Println(c)
 	fmt.Println("finish")
 }
 
-func read(rwMu *sync.RWMutex, wg *sync.WaitGroup, c *int) {
-	defer wg.Done()
-	time.Sleep(10 * time.Millisecond)
-	rwMu.RLock()
-	defer rwMu.RUnlock()
-	fmt.Println("read lock")
-	fmt.Println(*c)
-	time.Sleep(time.Second)
-	fmt.Println("read unlock")
+func deadlineTask(ctx context.Context) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		deadline, ok := ctx.Deadline()
+		if ok {
+			if deadline.Sub(time.Now().Add(30*time.Millisecond)) < 0 {
+				fmt.Println("impossible to meet deadline")
+				return
+			}
+		}
+		time.Sleep(30 * time.Millisecond)
+		ch <- "hello"
+	}()
+	return ch
 }
 
-func write(rwMu *sync.RWMutex, wg *sync.WaitGroup, c *int) {
-	defer wg.Done()
-	rwMu.Lock()
-	defer rwMu.Unlock()
-	fmt.Println("write lock")
-	*c = *c + 1
-	time.Sleep(time.Second)
-	fmt.Println("write unlock")
-}
+// func criticalTask(ctx context.Context) (string, error) {
+// 	_, cancel := context.WithTimeout(context.Background(), 1200*time.Millisecond)
+// 	defer cancel()
+// 	t := time.NewTicker(1000 * time.Millisecond)
+// 	select {
+// 	case <-ctx.Done():
+// 		return "", ctx.Err()
+// 	case <-t.C:
+// 		t.Stop()
+// 	}
+// 	return "A", nil
+// }
+
+// func normalTask(ctx context.Context) (string, error) {
+// 	t := time.NewTicker(3000 * time.Millisecond)
+// 	select {
+// 	case <-ctx.Done():
+// 		return "", ctx.Err()
+// 	case <-t.C:
+// 		t.Stop()
+// 	}
+// 	return "B", nil
+// }
+
+// func subTask(ctx context.Context, wg *sync.WaitGroup, id string) {
+// 	defer wg.Done()
+// 	t := time.NewTicker(500 * time.Millisecond)
+// 	select {
+// 	case <-ctx.Done():
+// 		fmt.Println(ctx.Err())
+// 		return
+// 	case <-t.C:
+// 		t.Stop()
+// 		fmt.Println(id)
+// 	}
+// }
