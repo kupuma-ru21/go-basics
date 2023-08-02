a5d0e38996769543a2e2d5b58b52d656f1012933
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-02 21:34:59 +0900

    chore: select continuous data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -12,27 +13,67 @@ import (
 // vscode goのextensionが正常に作動しなかった
 // https://formulae.brew.sh/formula/goplsをinstallする必要があった
 
-const bufSize = 3
+const bufSize = 5
 
 func main() {
+	ch1 := make(chan int, bufSize)
+	ch2 := make(chan int, bufSize)
 	var wg sync.WaitGroup
-	ch := make(chan string, bufSize)
-	wg.Add(1)
+	ctx, cancel := context.WithTimeout(context.Background(), 180*time.Millisecond)
+	defer cancel()
+	wg.Add(3)
+	go countProducer(&wg, ch1, bufSize, 50)
+	go countProducer(&wg, ch2, bufSize, 500)
+	go countConsumer(ctx, &wg, ch1, ch2)
+	wg.Wait()
+	fmt.Println("finish")
+}
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < bufSize; i++ {
-			time.Sleep(1000 * time.Millisecond)
-			ch <- "hello"
-		}
-	}()
-	for i := 0; i < 3; i++ {
+func countProducer(wg *sync.WaitGroup, ch chan<- int, size int, sleep int) {
+	defer wg.Done()
+	defer close(ch)
+	for i := 0; i < size; i++ {
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		ch <- i
+	}
+}
+
+func countConsumer(ctx context.Context, wg *sync.WaitGroup, ch1 <-chan int, ch2 <-chan int) {
+	defer wg.Done()
+	// loop:
+	for ch1 != nil || ch2 != nil {
 		select {
-		case m := <-ch:
-			fmt.Println(m)
-		default:
-			fmt.Println("no msg arrived")
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			// break loop
+			for ch1 != nil || ch2 != nil {
+				select {
+				case v, ok := <-ch1:
+					if !ok {
+						ch1 = nil
+						break
+					}
+					fmt.Printf("ch1 %v \n", v)
+				case v, ok := <-ch2:
+					if !ok {
+						ch2 = nil
+						break
+					}
+					fmt.Printf("ch2 %v \n", v)
+				}
+			}
+		case v, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+				break
+			}
+			fmt.Printf("ch1 %v \n", v)
+		case v, ok := <-ch2:
+			if !ok {
+				ch2 = nil
+				break
+			}
+			fmt.Printf("ch2 %v \n", v)
 		}
-		time.Sleep(1500 * time.Millisecond)
 	}
 }
