00b8eab9451bfaa398d62919bbaa91e740303236
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-03 00:23:16 +0900

    chore: errorGroup

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/goleak v1.2.1
 	golang.org/x/exp v0.0.0-20230728194245-b0cb94b80691
+	golang.org/x/sync v0.3.0
 )
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 // static checkをmanualで設定した。
@@ -13,105 +15,47 @@ import (
 // https://formulae.brew.sh/formula/goplsをinstallする必要があった
 
 func main() {
-	// var wg sync.WaitGroup
-	// ctx, cancel := context.WithTimeout(context.Background(), 400*time.Millisecond)
-	// defer cancel()
-	// wg.Add(3)
-	// go subTask(ctx, &wg, "a")
-	// go subTask(ctx, &wg, "b")
-	// go subTask(ctx, &wg, "c")
-	// wg.Wait()
-
-	// var wg sync.WaitGroup
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	v, err := criticalTask(ctx)
-	// 	if err != nil {
-	// 		fmt.Printf("critical task cancelled due to: %v\n", err)
-	// 		cancel()
-	// 		return
-	// 	}
-	// 	fmt.Println("success", v)
-	// }()
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	v, err := normalTask(ctx)
-	// 	if err != nil {
-	// 		fmt.Printf("normal task cancelled due to: %v\n", err)
-	// 		cancel()
-	// 		return
-	// 	}
-	// 	fmt.Println("success", v)
-	// }()
-
-	// wg.Wait()
-
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(20*time.Millisecond))
+	ctx, cancel := context.WithTimeout(context.Background(), 800*time.Millisecond)
 	defer cancel()
-	ch := deadlineTask(ctx)
-	v, ok := <-ch
-	if ok {
-		fmt.Println(v)
+
+	eg, ctx := errgroup.WithContext(ctx)
+	s := []string{"task1", "task2", "task3", "task4"}
+	for _, v := range s {
+		task := v
+		eg.Go(func() error { return doTask(ctx, task) })
+	}
+	if err := eg.Wait(); err != nil {
+		fmt.Printf("error %v \n", err)
 	}
 	fmt.Println("finish")
 }
 
-func deadlineTask(ctx context.Context) <-chan string {
-	ch := make(chan string)
-	go func() {
-		defer close(ch)
-		deadline, ok := ctx.Deadline()
-		if ok {
-			if deadline.Sub(time.Now().Add(30*time.Millisecond)) < 0 {
-				fmt.Println("impossible to meet deadline")
-				return
-			}
-		}
-		time.Sleep(30 * time.Millisecond)
-		ch <- "hello"
-	}()
-	return ch
+func doTask(ctx context.Context, task string) error {
+	var t *time.Ticker
+	switch task {
+	case "task1":
+		t = time.NewTicker(500 * time.Millisecond)
+	case "task2":
+		t = time.NewTicker(700 * time.Millisecond)
+	default:
+		t = time.NewTicker(1000 * time.Millisecond)
+
+	}
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%v canceled : %v\n", task, ctx.Err())
+		return ctx.Err()
+	case <-t.C:
+		t.Stop()
+		fmt.Printf("task %v completed\n", task)
+	}
+	return nil
 }
 
-// func criticalTask(ctx context.Context) (string, error) {
-// 	_, cancel := context.WithTimeout(context.Background(), 1200*time.Millisecond)
-// 	defer cancel()
-// 	t := time.NewTicker(1000 * time.Millisecond)
-// 	select {
-// 	case <-ctx.Done():
-// 		return "", ctx.Err()
-// 	case <-t.C:
-// 		t.Stop()
+// func oldDoTask(ctx context.Context, task string) error {
+// 	if task == "fake1" || task == "fake2" {
+// 		return fmt.Errorf("%v failed", task)
 // 	}
-// 	return "A", nil
-// }
-
-// func normalTask(ctx context.Context) (string, error) {
-// 	t := time.NewTicker(3000 * time.Millisecond)
-// 	select {
-// 	case <-ctx.Done():
-// 		return "", ctx.Err()
-// 	case <-t.C:
-// 		t.Stop()
-// 	}
-// 	return "B", nil
-// }
-
-// func subTask(ctx context.Context, wg *sync.WaitGroup, id string) {
-// 	defer wg.Done()
-// 	t := time.NewTicker(500 * time.Millisecond)
-// 	select {
-// 	case <-ctx.Done():
-// 		fmt.Println(ctx.Err())
-// 		return
-// 	case <-t.C:
-// 		t.Stop()
-// 		fmt.Println(id)
-// 	}
+// 	fmt.Printf("task %v completed\n", task)
+// 	return nil
 // }
